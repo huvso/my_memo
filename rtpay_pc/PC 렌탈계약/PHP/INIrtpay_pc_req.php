fd003b3d81b6c74f1d2d674bc3559b07b483e1d2
huvso <63341115+huvso@users.noreply.github.com>
2022-09-23 10:25:58 +0900

    Update INIrtpay_pc_req.php

--- rtpay_pc/PC 렌탈계약/PHP/INIrtpay_pc_req.php
+++ rtpay_pc/PC 렌탈계약/PHP/INIrtpay_pc_req.php
@@ -36,6 +36,10 @@
     
     String P_RESERVED = "rentalCompNm="+rentalCompNm+"&rentalCompNo="+rentalCompNo+"&rentalCompPhone="+rentalCompPhone+"&rentalRecipientNm="+rentalRecipientNm+"&rentalRecipientPhone="+rentalRecipientPhone;
 
+    /* 렌탈 상품 수령인 정보 설정 */
+    String P_RECV_POSTNUM = "00000";
+    String P_RECV_ADDR = "렌탈 상품 수령 기본 주소";
+    String P_RECV_ADDR_DETAIL = "렌탈 상품 수령 상세 주소";
 %>
     
 <!DOCTYPE html>
@@ -150,17 +154,17 @@
                             
                             <label class="col-10 col-sm-2 input param" style="border:none;">P_RECV_POSTNUM</label>
                             <label class="col-10 col-sm-9 input">
-                                <input type="text" name="P_RECV_POSTNUM" value="10266">
+                                <input type="text" name="P_RECV_POSTNUM" value="<%=P_RECV_POSTNUM %>">
                             </label>
                             
                             <label class="col-10 col-sm-2 input param" style="border:none;">P_RECV_ADDR</label>
                             <label class="col-10 col-sm-9 input">
-                                <input type="text" name="P_RECV_ADDR" value="중구 통일로 92 KG타워">
+                                <input type="text" name="P_RECV_ADDR" value="<%=P_RECV_ADDR %>">
                             </label>
                             
                             <label class="col-10 col-sm-2 input param" style="border:none;">P_RECV_ADDR_DETAIL</label>
                             <label class="col-10 col-sm-9 input">
-                                <input type="text" name="P_RECV_ADDR_DETAIL" value="14,15층">
+                                <input type="text" name="P_RECV_ADDR_DETAIL" value="<%=P_RECV_ADDR_DETAIL %>">
                             </label>
                             
                             <input type="hidden" name="additionalData" value="<%=encodeResult1 %>">
